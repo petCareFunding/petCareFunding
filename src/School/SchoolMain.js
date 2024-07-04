8493fcfaaabc4e3a713f2daf445aeebe4c12b88c
Daeuni <ekdmsjj@gmail.com>
2024-07-04 16:59:39 +0900

    schoolMain1

--- src/School/SchoolMain.js
+++ src/School/SchoolMain.js
@@ -1,1 +1,22 @@
 import React from "react";
+import './SchoolMain.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+function SchoolMain(){
+    return(
+        <div>
+            <header className="title">MEONG-GORITHM</header>
+            <div className="search-container">
+                <input type="text" className="search" placeholder="동네 검색하기.."/>
+                <img src="" alt="돋보기아이콘" className="searchbtn"></img>
+            </div>
+            <hr/>
+
+            <div className="container">
+
+            </div>
+        </div>
+    )
+};
+
+export default SchoolMain;
