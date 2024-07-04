ea608b26977920747bbf1ee309ee9851241661c6
xxseoxx <sjm4482@gmail.com>
2024-07-04 15:48:47 +0900

    3

--- src/login/Login_Page.js
+++ src/login/Login_Page.js
@@ -6,7 +6,9 @@ const Login_Page=()=>{
     return( 
         <>
             <div className="Login_page">
-                
+                <h2>로그인</h2>
+                <span>아이디 또는 이메일</span>
+
             </div>
 
 
