6c4f7138e8df95ee9a5a773fef1e4349dca8532b
xxseoxx <sjm4482@gmail.com>
2024-07-04 15:42:18 +0900

    2

--- src/login/Login_Page.js
+++ src/login/Login_Page.js
@@ -1,1 +1,21 @@
 import "../Login_Page.css";
+
+
+const Login_Page=()=>{
+
+    return( 
+        <>
+            <div className="Login_page">
+                
+            </div>
+
+
+        </>
+
+    )
+    
+
+
+}
+
+export default Login_Page;
