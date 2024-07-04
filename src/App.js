b9b23c1d46e73fd7879eec95372bc5fb9e2af7b5
Daeuni <ekdmsjj@gmail.com>
2024-07-04 15:38:17 +0900

    라우터 임포트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
---
+++ src/MainPage/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import './Header.css';
+
+function MainHeader(){
+    return(
+        <div>
+            <header></header>
+        </div>
+    )
+}
+
+
+    
