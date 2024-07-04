c938d1d363cdc25b9b54d7ca9b851fdd38f9995e
xxseoxx <sjm4482@gmail.com>
2024-07-04 16:19:35 +0900

    4

--- src/login/Login_Page.js
+++ src/login/Login_Page.js
@@ -7,8 +7,26 @@ const Login_Page=()=>{
         <>
             <div className="Login_page">
                 <h2>로그인</h2>
-                <span>아이디 또는 이메일</span>
 
+                <div className="login_input">
+                    <div>아이디 또는 이메일</div>
+                    <input type="text" placeholder="Email" />
+                    <div>비밀번호</div>
+                    <input type="password" placeholder="Password" />
+                    <div>아이디/비밀번호 찾기</div>
+                </div>
+                <div className="submit_button">
+                    <input type="submit" value="로그인" />
+                </div>
+                <div>또는</div>
+                <div className="kakao_button">
+                <input type="submit" value="카카오 로그인" />
+                </div>
+                <div className="naver_button">
+                <input type="submit" value="네이버 로그인" />
+                </div>
+
+                
             </div>
 
 
