727ee9224a57eeb62549b7f8b198282aa0a740d9
Daeuni <ekdmsjj@gmail.com>
2024-07-04 17:02:53 +0900

    schMain1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react';
+import SchoolMain from './School/SchoolMain.js';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <SchoolMain/>
     </div>
+   
+    
   );
 }
 
