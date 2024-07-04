14ab7e2e4557aa8fec1ace0439430cf00808c2e7
조민성 <fjkg33@gmail.com>
2024-07-04 14:45:11 +0900

    PetHader와PetItemspage 수정

---
+++ src/PetProductsPage/Header/PetHeader.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'PetHeader.css'
+
+const PetHeader = () => {
+  return (
+    <div>
+        <h1>HAVE A PUPPY DAY</h1>
+        <from>
+            <input type='text' placeholder='용품검색...'></input>
+        </from>
+    </div>
+  )
+}
+
+export default PetHeader
---
+++ src/PetProductsPage/PetItemsPage/PetItemspage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PetHeader from '../Header/PetHeader'
+
+const PetItemspage = () => {
+  return (
+    <div>
+      <PetHeader/>
+    </div>
+  )
+}
+
+export default PetItemspage
