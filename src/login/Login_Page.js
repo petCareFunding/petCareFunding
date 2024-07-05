9aa723edbfe202db04ce75d5d0251291c3a1c877
xxseoxx <sjm4482@gmail.com>
2024-07-05 10:09:01 +0900

    6

--- src/login/Login_Page.js
+++ src/login/Login_Page.js
@@ -6,26 +6,30 @@ const Login_Page=()=>{
     return( 
         <>
             <div className="Login_page">
-                <h2>로그인</h2>
+                <div className="login-wrapper">
+                    <h2>로그인</h2>
 
-                <div className="login_input">
-                    <div>아이디 또는 이메일</div>
-                    <input type="text" placeholder="Email" />
-                    <div>비밀번호</div>
-                    <input type="password" placeholder="Password" />
-                    <div>아이디/비밀번호 찾기</div>
-                </div>
-                <div className="submit_button">
-                    <input type="submit" value="로그인" />
-                </div>
-                <div>또는</div>
-                <div className="kakao_button">
-                <input type="submit" value="카카오 로그인" />
-                </div>
-                <div className="naver_button">
-                <input type="submit" value="네이버 로그인" />
-                </div>
+                    <div className="login_input">
+                        <div>아이디 또는 이메일</div>
+                        <input type="text" placeholder="Email" />
+                        <div>비밀번호</div>
+                        <input type="password" placeholder="Password" />
+                        <div>아이디/비밀번호 찾기</div>
+                    </div>
+                    <div className="submit_button">
+                        <input type="submit" value="로그인" />
+                    </div>
+                    <div>또는</div>
+                    <div className="kakao_button">
+                        <a href="#"><input type="submit" value="카카오 로그인" /></a>
+                    </div>
+                    <div className="naver_button">
+                        <a href="#"><input type="submit" value="네이버 로그인" /></a>
+                    </div>
+                    <div>회원이 아니시라면?</div>
+                    <a href="#"><div>회원가입</div></a>
 
+                </div>
                 
             </div>
 
