f945758caa342115faf3dbd9ceddae271bf175ae
xxseoxx <sjm4482@gmail.com>
2024-07-05 16:58:27 +0900

    2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import React from 'react';
 import Main from './MainPage/Main';
 import Login_Page from './login/Login_Page';
-import { Routes, Route,Link } from 'react-router-dom';
+import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      
+      <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login_Page />} />
       </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
