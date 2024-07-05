9e9afc02b63a55bcc20371c84e1eaf744074e9e4
Daeuni <ekdmsjj@gmail.com>
2024-07-05 22:44:36 +0900

    0705(2)

--- src/School/SchoolMain.js
+++ src/School/SchoolMain.js
@@ -17,6 +17,7 @@ function SchoolMain(){
             <div className="search-container">
                 <input type="text" className="search" placeholder="지역 검색하기.."/>
                 <img src={process.env.PUBLIC_URL + './ion_search.png'} alt="돋보기아이콘" className="searchbtn"></img>
+                <button id="postbtn">펀딩 등록하기</button>
             </div>
             <hr/>
             <main className="container">
@@ -37,9 +38,23 @@ function SchoolMain(){
                     </div>)
                 })}
             </main>
-            <footer>
-                <div className="footer">
-                    <h2 className="title-footer">MEONG-GORITHM</h2>
+            <footer className="footer">
+                <div className="footerleft">
+                    <p>환불규정</p>
+                    <p>이 상품에 대한 환불은 어쩌구저쩌구 처리해서 이렇게 저렇게 할 거고 
+                        <br/>그 다음엔 요로케 저러케  할 예정입니다</p>
+                </div>
+                <div className="footercenter">
+                    <p>서비스 이용약관<br/>
+                        마이데이터 서비스 이용약관<br/>
+                        개인정보 처리방침<br/>
+                        가맹점 고지사항<br/>
+                        위치기반서비스 이용약관</p>
+                </div>
+                <div className="footerRight>">
+                    <h3>Follow us</h3>
+                    <img src={process.env.PUBLIC_URL + './insta.png'} className="sns"></img>
+                    <img src={process.env.PUBLIC_URL + './facebook.png'} className="sns"></img>
                 </div>
             </footer>     
         </div>
