431bff88d7e7fbf0b93bc2ba29068c2c60523fa1
Daeuni <ekdmsjj@gmail.com>
2024-07-07 02:43:33 +0900

    유치원메인페이지

--- src/School/SchoolMain.js
+++ src/School/SchoolMain.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import './SchoolMain.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import SchoolPost from "./SchoolPost.js";
+
+
 
 
 function SchoolMain(){
@@ -11,13 +15,18 @@ function SchoolMain(){
     var [month,setMonth] = useState([7,8,9,9]);
     var [date,setDate] = useState([22,6,16,13]);
     var [like,setLike] = useState([10,60,45,5]);
+
+    const navigate = useNavigate();
+    const Gopostpage = ()=>{navigate('/school/postSchoolFunding')};
     return(
         <div>
             <header className="title">MEONG-GORITHM</header>
             <div className="search-container">
                 <input type="text" className="search" placeholder="지역 검색하기.."/>
                 <img src={process.env.PUBLIC_URL + './ion_search.png'} alt="돋보기아이콘" className="searchbtn"></img>
-                <button id="postbtn">펀딩 등록하기</button>
+            </div>
+            <div className="postbtn-container">
+                <button id="postbtn" onClick={Gopostpage}>펀딩 등록하기</button>
             </div>
             <hr/>
             <main className="container">
@@ -31,7 +40,7 @@ function SchoolMain(){
                             <h2>{title[i]}</h2>
                             <p>펀딩인원     <span className="fund People">{people[i]}/20명</span></p>
                             <p>펀딩기간     <span className="fund Date">{month[i]}월{date[i]}일</span></p>
-                            <p id="heart">❤️{like[i]}</p>
+                            <p id="heart">❤️ 현재 {like[i]}명이 찜했어요!</p>
                         </div>
                         </section>
                         <hr/>
@@ -39,19 +48,19 @@ function SchoolMain(){
                 })}
             </main>
             <footer className="footer">
-                <div className="footerleft">
+                <div className="footer-text first">
                     <p>환불규정</p>
-                    <p>이 상품에 대한 환불은 어쩌구저쩌구 처리해서 이렇게 저렇게 할 거고 
+                    <p><br/>이 상품에 대한 환불은 어쩌구저쩌구 처리해서 이렇게 저렇게 할 거고 
                         <br/>그 다음엔 요로케 저러케  할 예정입니다</p>
                 </div>
-                <div className="footercenter">
+                <div className="footer-text second">
                     <p>서비스 이용약관<br/>
                         마이데이터 서비스 이용약관<br/>
                         개인정보 처리방침<br/>
                         가맹점 고지사항<br/>
                         위치기반서비스 이용약관</p>
                 </div>
-                <div className="footerRight>">
+                <div className="footer-text">
                     <h3>Follow us</h3>
                     <img src={process.env.PUBLIC_URL + './insta.png'} className="sns"></img>
                     <img src={process.env.PUBLIC_URL + './facebook.png'} className="sns"></img>
@@ -65,6 +74,5 @@ function SchoolMain(){
 
 
 
-    
 
 export default SchoolMain;
