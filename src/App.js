7252be48583073ab76ec28ebe2dc2639f5e28a92
xxseoxx <sjm4482@gmail.com>
2024-07-08 14:33:49 +0900

    버튼 디자인

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import React from 'react';
 import Main from './MainPage/Main';
 import Login_Page from './login/Login_Page';
 import Join_Page from './join_members/Join_Page';
+import JoinComplete_Page from './JoinComplete_Page/JoinComplete_Page';
 import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login_Page />} />
-          <Route path="/join_members" element={<Join_Page />} />
-        </Routes>
-      </BrowserRouter>
+      <JoinComplete_Page/>
     </div>
   );
 }
