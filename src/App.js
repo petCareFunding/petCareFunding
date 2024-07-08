262b9b6ded73ac31ca03ced2f867c73fe8cbe603
조민성 <fjkg33@gmail.com>
2024-07-08 14:50:26 +0900

    Update App.js
    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import React from "react";
+import Main from './MainPage/Main';
+import Login_Page from './login/Login_Page';
+import Join_Page from './join_members/Join_Page';
 import ItemsPage from "./PetProductsPage/PetItemspage/ItemsPage";
 import ItemDetail from "./PetProductsPage/ItemDetailPage/ItemDetail";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -8,6 +11,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login_Page />} />
+          <Route path="/join_members" element={<Join_Page />} />
         <Route path="/ItemsPage" element={<ItemsPage />} />
         <Route path="/ItemsPage/:imageId" element={<ItemDetail />} />
       </Routes>
