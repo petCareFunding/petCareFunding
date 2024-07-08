7cc57aaea525b0a46e9554d1eb330a9607c44791
xxseoxx <sjm4482@gmail.com>
2024-07-08 11:02:45 +0900

    회원가입 홈페이지 완성

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import React from 'react';
 import Main from './MainPage/Main';
 import Login_Page from './login/Login_Page';
+import Join_Page from './join_members/Join_Page';
 import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login_Page />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login_Page />} />
+          <Route path="/join_members" element={<Join_Page />} />
+        </Routes>
       </BrowserRouter>
-      
     </div>
   );
 }
--- src/join_members/Join_Page.js
+++ src/join_members/Join_Page.js
@@ -1,1 +1,53 @@
-1234567
+import "./Join_Page.css";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { SiNaver } from "react-icons/si";
+
+const Join_Page=()=>{
+    return(
+        <>
+              <div className="Join_page">
+                <div className="join-wrapper">
+                    <h2 className="join">회원가입</h2>
+                    <div className="email">아이디 또는 이메일</div>
+                    <div className="Email_input">
+                        <input type="text" placeholder="이메일 입력" />
+                   </div>
+                   <div className="emailCheck_button">
+                        <input type="submit" value="인증하기"/>
+                   </div>
+                   <div className="name">이름</div>
+                    <div className="name_input">
+                        <input type="name" placeholder="이름 입력" />
+                    </div>
+                    <div className="password">비밀번호</div>
+                    <div className="Password_input">
+                        <input type="password" placeholder="비밀번호 입력" />
+                    </div>
+                    <div className="passwordCheck">비밀번호 확인</div>
+                    <div className="PasswordCheck_input">
+                        <input type="password" placeholder="비밀번호 확인" />
+                    </div>
+                    
+                    <div className="submit_button">
+                        <input type="submit" value="가입 완료하기" />
+                    </div>
+                    <div className="easyJoin">간편가입</div>
+                    <div className="kakao-img"><RiKakaoTalkFill size="35px"/></div>
+                    <div className="kakao_button">
+                    <a href="#"><input type="submit" value=" 카카오로 회원가입" /></a>
+                    </div>
+                    <div className="naver-img"><SiNaver size="29px"/></div>
+                    <div className="naver_button">
+                        <a href="#"><input type="submit" value="네이버로 회원가입" /></a>
+                    </div>
+                    
+                    
+                    
+
+                </div>
+                
+            </div>
+        </>
+    )
+}
+export default Join_Page;
