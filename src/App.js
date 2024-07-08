4cb0e2e6e6a038fd31fc343f896c07c6115e3eac
xxseoxx <sjm4482@gmail.com>
2024-07-08 14:47:51 +0900

    app.js수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import PetItemspage from './PetProductsPage/PetItemsPage/PetItemspage';
 import MyPage from './Mypage/Mypage';
 import Header from './Mypage/Header/Header';
 import { Routes, Route,Link } from 'react-router-dom';
