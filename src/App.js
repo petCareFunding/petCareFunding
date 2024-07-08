1e44b9ed31c28c65d56477d0e17cc2ab7a257fce
xxseoxx <sjm4482@gmail.com>
2024-07-08 16:16:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Main from './MainPage/Main';
 import Login_Page from './login/Login_Page';
 import Join_Page from './join_members/Join_Page';
+import SchoolMain from './School/SchoolMain.js';
+import SchoolPost from './School/SchoolPost.js';
 import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login_Page />} />
           <Route path="/join_members" element={<Join_Page />} />
+          <Route path='/school' element={<SchoolMain/>}></Route>
+          <Route path='/school/postSchoolFunding' element={<SchoolPost/>}/>
         </Routes>
       </BrowserRouter>
     </div>
