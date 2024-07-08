354cdfd8148c534dc0884d09d49a3126b83aed61
김용훈 <magic0224121@naver.com>
2024-07-08 14:44:19 +0900

    app.js 추가

---
+++ src/Mypage/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyPage from './components/MyPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MyPage />
+    </div>
+  );
+}
+
+export default App;
