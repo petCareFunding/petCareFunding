09330919f40c75696272f8e8f8fa950efb6033b3
xxseoxx <sjm4482@gmail.com>
2024-07-08 11:25:01 +0900

    회원가입페이지 디자인완성 및 연결

--- src/join_members/Join_Page.js
+++ src/join_members/Join_Page.js
@@ -7,9 +7,9 @@ const Join_Page=()=>{
         <>
               <div className="Join_page">
                 <div className="join-wrapper">
-                    <h2 className="join">회원가입</h2>
-                    <div className="email">아이디 또는 이메일</div>
-                    <div className="Email_input">
+                    <h2 className="join_Title">회원가입</h2>
+                    <div className="j_email">아이디 또는 이메일</div>
+                    <div className="joinEmail_input">
                         <input type="text" placeholder="이메일 입력" />
                    </div>
                    <div className="emailCheck_button">
@@ -19,25 +19,25 @@ const Join_Page=()=>{
                     <div className="name_input">
                         <input type="name" placeholder="이름 입력" />
                     </div>
-                    <div className="password">비밀번호</div>
-                    <div className="Password_input">
+                    <div className="joinPassword">비밀번호</div>
+                    <div className="joinPassword_input">
                         <input type="password" placeholder="비밀번호 입력" />
                     </div>
-                    <div className="passwordCheck">비밀번호 확인</div>
+                    <div className="joinpasswordCheck">비밀번호 확인</div>
                     <div className="PasswordCheck_input">
                         <input type="password" placeholder="비밀번호 확인" />
                     </div>
                     
-                    <div className="submit_button">
+                    <div className="joinSubmit_button">
                         <input type="submit" value="가입 완료하기" />
                     </div>
                     <div className="easyJoin">간편가입</div>
-                    <div className="kakao-img"><RiKakaoTalkFill size="35px"/></div>
-                    <div className="kakao_button">
+                    <div className="join_kakao-img"><RiKakaoTalkFill size="35px"/></div>
+                    <div className="joinKakao_button">
                     <a href="#"><input type="submit" value=" 카카오로 회원가입" /></a>
                     </div>
-                    <div className="naver-img"><SiNaver size="29px"/></div>
-                    <div className="naver_button">
+                    <div className="join_naver-img"><SiNaver size="29px"/></div>
+                    <div className="joinNaver_button">
                         <a href="#"><input type="submit" value="네이버로 회원가입" /></a>
                     </div>
                     
