8225b257764aec6fe6b29c82a980554d982186ec
Daeuni <ekdmsjj@gmail.com>
2024-07-09 13:48:05 +0900

    정보수정페이지 브랜치 생성

---
+++ src/Mypage/EditInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './EditInfo.css';
+
+function EditInfo(){
+    return(
+        <div>
+            
+        </div>
+    )
+};
+export default EditInfo;
