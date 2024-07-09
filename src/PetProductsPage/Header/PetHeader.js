d8220c3120ab3ff76e7afe8c02ebbfea1a850578
조민성 <fjkg33@gmail.com>
2024-07-09 15:19:08 +0900

    header&footer 수정

--- src/PetProductsPage/Header/PetHeader.js
+++ src/PetProductsPage/Header/PetHeader.js
@@ -2,11 +2,12 @@ import React from 'react'
 import '../Header/PetHeader.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch} from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 export default function PetHeader(){
   return(
     <div className='header'>
-        <h1>HAVE A PUPPY DAY</h1>
+        <Link to="/"><h1>HAVE A PUPPY DAY</h1></Link>
             <input type='text' placeholder='용품 검색'></input>
             <FontAwesomeIcon icon={faSearch}  className='icon'/>
     </div>
