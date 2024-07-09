10d2bb5f8dae540eab318ade58addc673b23b2d1
김용훈 <magic0224121@naver.com>
2024-07-09 09:39:37 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import React from 'react';
-import MyPage from './Mypage/Mypage';
-import Header from './Mypage/Header/Header';
-import { Routes, Route,Link } from 'react-router-dom';
+import Main from './MainPage/Main';
+import Login_Page from './login/Login_Page';
+import Join_Page from './join_members/Join_Page';
+import SchoolMain from './School/SchoolMain.js';
+import SchoolPost from './School/SchoolPost.js';
+import Mypage from './Mypage/Mypage.js';
+import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MyPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login_Page />} />
+          <Route path="/join_members" element={<Join_Page />} /> 
+          <Route path='/school' element={<SchoolMain/>}></Route>
+          <Route path='/school/postSchoolFunding' element={<SchoolPost/>}/>
+          <Route path='/Mypage' element={<Mypage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
