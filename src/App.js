724a3b0ea32490e086558c5b88bf7d95d4944231
xxseoxx <sjm4482@gmail.com>
2024-07-09 10:04:00 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
-import ItemsPage from "./PetProductsPage/PetItemsPage/ItemsPage";
-import ItemDetail from "./PetProductsPage/ItemDetailPage/ItemDetail";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from './MainPage/Main';
 import Login_Page from './login/Login_Page';
 import Join_Page from './join_members/Join_Page';
 import SchoolMain from './School/SchoolMain.js';
 import SchoolPost from './School/SchoolPost.js';
+import ItemsPage from "./PetProductsPage/PetItemsPage/ItemsPage";
+import ItemDetail from "./PetProductsPage/ItemDetailPage/ItemDetail";
 import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
 
 function App() {
