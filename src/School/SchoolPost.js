aac64296d6fce0f4e1e037819fba8f221010b065
Daeuni <ekdmsjj@gmail.com>
2024-07-11 23:13:21 +0900

    dom->상태변환으로 코드 작성

--- src/School/SchoolPost.js
+++ src/School/SchoolPost.js
@@ -2,30 +2,32 @@ import './SchoolPost.css';
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-function SchoolPost(){
-    var [show,setShow] = useState(false);
-    const [photoURL, setPhotoURL] = useState('');
-    const showAlert = ()=>{
-        const uploadPhoto = document.querySelector('#uploadPhoto').value;
-        const title = document.querySelector('#title').value;
-        const person = document.querySelector('#person').value;
-        const fundingDate1 = document.querySelector('#fundingDate1').value;
-        const fundingDate2 = document.querySelector('#fundingDate2').value;
-        const info = document.querySelector('#info').value;
 
-        if(!uploadPhoto|| !title|| !person|| ! fundingDate1|| !fundingDate2|| !info){
+
+function SchoolPost(){
+    
+        const [show, setShow] = useState(false);
+        const [title, setTitle] = useState('');
+        const [person, setPerson] = useState('');
+        const [fundingDate1, setFundingDate1] = useState('');
+        const [fundingDate2, setFundingDate2] = useState('');
+        const [info, setInfo] = useState('');
+    
+        const handleSubmit = ()=>{
+        if( !title|| !person|| ! fundingDate1|| !fundingDate2|| !info){
             setShow(true);
             alert('모든 칸을 채워주세요');
         }
         else{
             alert('펀딩이 등록되었습니다');
         }
-    };
+        };
     
     const navigate = useNavigate();
     const Backschool = ()=>{navigate('/school')};
 
-    
+   
+
 
     return(
         <div className='container schoolpost'>
@@ -35,7 +37,8 @@ function SchoolPost(){
                     <label className='photo-fill-btn' htmlFor = 'uploadPhoto'>
                       사진 업로드
                     </label>
-                    <input type='file' accept='.png, .jpeg, .jpg' id='uploadPhoto' style={{display:'none'}} required/> 
+                    <input type='file' accept='image/*' style={{display:'none'}} 
+                         multiple required/> 
                     <div className='photo-container'>
                         <div className= 'selectedbox' id='selected1'></div>           
                         <div className= 'selectedbox' id='selected2'></div>           
@@ -46,23 +49,28 @@ function SchoolPost(){
             </div>
             <div id='title-container'>
                     <h3>글 제목</h3>
-                    <input type='text' id='title' placeholder='지역 + 유치원이름'></input>
+                    <input type='text' id='title' placeholder='지역 + 유치원이름' 
+                     value = {title} onChange={(e)=> setTitle(e.target.value)}></input>
             </div>
             <div id='person-container'>
                     <h3>판매자 정보</h3>
-                    <input id='person' placeholder='유치원이름 기재'></input>
+                    <input id='person' placeholder='유치원이름 기재' 
+                      value = {person} onChange={(e)=> setPerson(e.target.value)}></input>
             </div>
             <div id='date-container'>
                     <h3>펀딩기간</h3>
-                    <input type='date' id='fundingDate1'></input> 
+                    <input type='date' id='fundingDate1' 
+                      value = {fundingDate1} onChange={(e)=> setFundingDate1(e.target.value)}></input> 
                     <h2>~</h2>
-                    <input type='date' id='fundingDate2'></input>
+                    <input type='date' id='fundingDate2' 
+                      value = {fundingDate2} onChange={(e)=> setFundingDate2(e.target.value)}></input>
             </div>
             <div id='info-container'>
                     <h3>상품설명</h3>
-                    <textarea id='info' placeholder='상품에 대해 설명해주세요..'></textarea>
+                    <textarea id='info' placeholder='상품에 대해 설명해주세요..' 
+                      value = {info} onChange={(e)=> setInfo(e.target.value)}></textarea>
             </div>
-            <button id='uploadbtn' onClick={showAlert}>등록하기</button>
+            <button id='uploadbtn' onClick={handleSubmit}>등록하기</button>
             <button id='uploadbtn' onClick={Backschool}>글 목록</button>
         </div>
     )
