88ad21f28dadb92e342de83f462b38c7d4a471a9
Daeuni <ekdmsjj@gmail.com>
2024-07-11 16:06:26 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React from 'react';
-// import Main from './MainPage/Main';
-// import Login_Page from './login/Login_Page';
-// import Join_Page from './join_members/Join_Page';
+//import Main from './MainPage/Main';
+//import Login_Page from './login/Login_Page';
+//import Join_Page from './join_members/Join_Page';
 import SchoolMain from './School/SchoolMain.js';
 import SchoolPost from './School/SchoolPost.js';
 import { Routes, Route,Link, BrowserRouter } from 'react-router-dom';
@@ -12,9 +12,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Main />} />
+          {/*<Route path="/" element={<Main />} />
           <Route path="/login" element={<Login_Page />} />
-          <Route path="/join_members" element={<Join_Page />} />  */}
+          <Route path="/join_members" element={<Join_Page />} /> */}
           <Route path='/school' element={<SchoolMain/>}></Route>
           <Route path='/school/postSchoolFunding' element={<SchoolPost/>}/>
         </Routes>
