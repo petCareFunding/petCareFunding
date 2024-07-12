6095c82ca0ab36f963971d722ef28bf544518ec5
Daeuni <ekdmsjj@gmail.com>
2024-07-13 00:06:45 +0900

    이미지 업로드 map함수 작성 전 코드

--- src/School/SchoolPost.js
+++ src/School/SchoolPost.js
@@ -6,27 +6,48 @@ import { useNavigate } from 'react-router-dom';
 
 function SchoolPost(){
     
-        const [show, setShow] = useState(false);
-        const [title, setTitle] = useState('');
-        const [person, setPerson] = useState('');
-        const [fundingDate1, setFundingDate1] = useState('');
-        const [fundingDate2, setFundingDate2] = useState('');
-        const [info, setInfo] = useState('');
+    const [show, setShow] = useState(false);
+    const [title, setTitle] = useState('');
+    const [person, setPerson] = useState('');
+    const [fundingDate1, setFundingDate1] = useState('');
+    const [fundingDate2, setFundingDate2] = useState('');
+    const [info, setInfo] = useState('');
     
-        const handleSubmit = ()=>{
+    const handleSubmit = ()=>{
         if( !title|| !person|| ! fundingDate1|| !fundingDate2|| !info){
             setShow(true);
             alert('모든 칸을 채워주세요');
         }
         else{
             alert('펀딩이 등록되었습니다');
-        }
         };
+    }; 
     
     const navigate = useNavigate();
     const Backschool = ()=>{navigate('/school')};
 
-   
+    const [postImg, setPostImg] = useState([]); //이미지 파일 자체의 상태
+    const [preview, setPreview] = useState([]); //이미지 파일의 url 담기 
+
+
+    const handleUpload = (e)=>{
+        const fileArr = e.target.files;
+        setPostImg(Array.from(fileArr)); //배열로 만들어서 state에 업데이트
+        
+        const fileUrl = []; //url 넣어줄 빈 배열 생성
+        //파일의 개수만큼 반복문 실행
+
+        for(let i =0; i < fileArr.length; i++){
+            let reader = new FileReader(); //데이터 읽기
+            reader.onload = ()=>{          //읽은 데이터에 이벤트걸어서 데이터를 문자열로 반환하게함(콜백함수)
+                fileUrl[i] = reader.result;
+                setPreview([...fileUrl]);
+                reader.readAsDataURL(fileArr[i]);
+            }
+        }
+    };
+
+        
 
 
     return(
@@ -74,8 +95,8 @@ function SchoolPost(){
             <button id='uploadbtn' onClick={Backschool}>글 목록</button>
         </div>
     )
+    
+
 };
-
-
 
 export default SchoolPost;
