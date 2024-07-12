fc8ca3559e638cec088fa46a1cd77a62c95073c8
simqlrud <166906722+simqlrud@users.noreply.github.com>
2024-07-12 16:08:10 +0900

    funeral main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import React from 'react';
-import PetItemspage from './PetProductsPage/PetItemsPage/PetItemspage';
-import { Routes, Route,Link } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-      <PetItemspage/>
+      
+  
+
     </div>
+
+
   );
 }
 
