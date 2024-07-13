be126ac7f91990f8052b9a1ae3f160950b5a8079
조민성 <fjkg33@gmail.com>
2024-07-13 10:31:54 +0900

    결제페이지 목업데이터 추가
    목업데이터 추가

---
+++ src/PetProductsPage/PayMentPage/PayMockup.js
@@ -0,0 +1,37 @@
+const PayMockup = {
+  order: {
+    name: "조민성",
+    phone: "010-1234-5678",
+    email: "test@example.com",
+    products: [
+      {
+        id: 1,
+        imeageURL: "https://cdn3.wadiz.kr/studio/images/2024/06/13/92daa32e-e953-4f56-ab36-4ce4191d1b5e.jpeg/wadiz/resize/800/format/jpg/quality/85/",
+        name: "[반려견 눈물자국 개선 프로젝트] 저알러지 사료+덴탈껌+트릿 세트!",
+        quantity: 2,
+        price: 30900,
+      },
+      {
+        id: 2,
+        imeageURL: "https://cdn3.wadiz.kr/studio/images/2024/06/04/16f78a9e-930b-4bab-b4fa-1ea6466358b1.jpeg/wadiz/resize/800/format/jpg/quality/85/",
+        name: "딸깍. 켜놓고 출근 하면 끝 | 이제 고양이들의 사냥파티 시작",
+        quantity: 1,
+        price: 39000,
+      },
+      {
+        id: 3,
+        imeageURL:"https://cdn3.wadiz.kr/studio/images/2024/06/06/1790acdb-7c53-4133-80f0-84d1fbea6fe7.jpeg/wadiz/resize/800/format/jpg/quality/85/MockupImg/Mlik.jpga",
+        name: "[5차 신규입고 예약할인] 펫츠온 호주 프리미엄 펫밀크 독앤퍼피/캣앤키튼",
+        quantity: 1,
+        price: 39900,
+      },
+    ],
+    shippingAddress: "부산광역시 사상구 덕포동",
+    delivery: "무료배송",
+    pay: "신용카드",
+    DeliveryRequest: "문앞에 놔두어 주세요",
+    totalPrice: 39900,
+  },
+};
+
+export default PayMockup;
