985434ea027f845d9248f94a03595d85ca0f93b7
김용훈 <magic0224121@naver.com>
2024-07-13 09:35:38 +0900

    찜 목록 이미지 추가

--- src/Wishlist/Wishlist.js
+++ src/Wishlist/Wishlist.js
@@ -10,9 +10,9 @@ const WishlistItem = ({ image, title }) => (
 
 const Wishlist = () => {
   const items = [
-    <img src="/" />, {title: '유치원 찜 목록'},
-    <img src="/" />, {title: '용품 찜 목록' },
-    <img src="/" />, {title: '컨텐츠 찜 목록' },
+    <img src="/Wishlist_img/kindergarten.png" />, {title: '유치원 찜 목록'},
+    <img src="/Wishlist_img/Funding.png" />, {title: '용품 찜 목록' },
+    <img src="/Wishlist_img/Supplies.png" />, {title: '컨텐츠 찜 목록' },
   ];
 
   return (
