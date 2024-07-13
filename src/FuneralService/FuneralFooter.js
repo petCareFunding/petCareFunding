b0b723a118a7831326a920d8e5fd4f5766dafc6f
simqlrud <166906722+simqlrud@users.noreply.github.com>
2024-07-13 11:38:42 +0900

    장례 메인 페이지 classname 수정

--- src/FuneralService/FuneralFooter.js
+++ src/FuneralService/FuneralFooter.js
@@ -8,21 +8,21 @@ function FFooter(){
 
     return(
         <>
-        <section className="footer">
-        <div className="footer-text1">
+        <section className="Ffooter">
+        <div className="Ffooter-text1">
                     <p>환불규정</p>
                     <p><br/>이 상품에 대한 환불은 어쩌구저쩌구 처리해서 이렇게 저렇게 할 거고 
                         <br/>그 다음엔 요로케 저러케  할 예정입니다</p>
                 </div>
-                <div className="footer-text2">
+                <div className="Ffooter-text2">
                     <p>서비스 이용약관<br/>
                         마이데이터 서비스 이용약관<br/>
                         개인정보 처리방침<br/>
                         가맹점 고지사항<br/>
                         위치기반서비스 이용약관</p>
                 </div>
-                <div className="footer-text">
-                    <div className="Follow">Follow us</div>
+                <div className="Ffooter-text">
+                    <div className="FFollow">Follow us</div>
                     <img src={process.env.PUBLIC_URL + './F_img/insta.png'} className="Fsns"></img>
                     <img src={process.env.PUBLIC_URL + './F_img/facebook.png'} className="Fsns"></img>
                 </div>
