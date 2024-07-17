8e3b36fc07f26a51d17d912de2060674edac765e
Daeuni <ekdmsjj@gmail.com>
2024-07-17 16:37:33 +0900

    클래스이름 중복되지않게 변경

--- src/Mypage/EditInfo.js
+++ src/Mypage/EditInfo.js
@@ -6,6 +6,8 @@ function EditInfo() {
 
     
     const [imageSrc, setImageSrc] = useState(null);
+    const [befpassword, setBefpassword] = useState('');
+    const [aftpassword, setAftpassword] = useState('');
 
     const handleImageChange = (e)=>{
         const reader = new FileReader();
@@ -16,8 +18,6 @@ function EditInfo() {
         }
     };
     
-    const [befpassword, setBefpassword] = useState('');
-    const [aftpassword, setAftpassword] = useState('');
 
 
     const handleBeforepw = (e)=>{
@@ -39,19 +39,19 @@ function EditInfo() {
 
     return (
         <div className="EditMyInfo">
-            <header className="info-title">
+            <header className="myinfo-title">
                 MEONG-GORITHM
             </header>
-            <div className="form-container">
-                <div className="form-section">
-                    <div className="form-sub">
+            <div className="myinfo-form-container">
+                <div className="myinfo-form-section">
+                    <div className="myinfo-form-sub">
                         <label>이름</label>
                         <input type="text" maxLength="10" />
                     </div>
 
-                    <div className="form-sub">
+                    <div className="myinfo-form-sub">
                         <label>전화번호</label>
-                        <div className="phone-number">
+                        <div className="myinfo-phone-number">
                             <input type="text" maxLength="3" placeholder="010" />
                             <span>-</span>
                             <input type="text" maxLength="4" placeholder="0000" />
@@ -60,34 +60,34 @@ function EditInfo() {
                         </div>
                     </div>
 
-                    <div className="form-sub">
+                    <div className="myinfo-form-sub">
                         <label>주소</label>
                         <input type="text" placeholder="우편번호" maxLength='5'/>
                         <input type="text" placeholder="상세 주소(동, 호)" />
                     </div>
 
-                    <div className="form-sub">
+                    <div className="myinfo-form-sub">
                         <label>닉네임</label>
                         <input type="text" maxLength="10" placeholder="10글자 이하" />
                     </div>
 
-                    <div className="form-sub">
+                    <div className="myinfo-form-sub">
                         <label>비밀번호</label>
                         <input type="text" placeholder="영어 소문자+숫자 포함 6자 이상" 
                          value={befpassword}  onChange={handleBeforepw} id='beforepassword' />
                     </div>
 
-                    <div className="form-sub">
+                    <div className="myinfo-form-sub">
                         <label>비밀번호 확인</label>
                         <input type="password" placeholder="비밀번호 확인" id='checkpassword'
                           value={aftpassword}   onChange={handleAfterpw} />
-                        <button id='cpwbtn' onClick={handlePwChange}>비밀번호 확인</button>
+                        <button id='myinfo-cpwbtn' onClick={handlePwChange}>비밀번호 확인</button>
                     </div>
                 </div>
-                <div className="profile-section">
+                <div className="myinfo-profile-section">
                     <div id='myprofile-container'>
-                        <div className='circle'>{<img src = {imageSrc ? imageSrc : null } id='circle'/>}</div>
-                        <label className='pickphoto' htmlFor='myprofile'>사진 선택하기</label>
+                        <div className='myinfo-circle'>{<img src = {imageSrc ? imageSrc : null } id='myinfo-circle'/>}</div>
+                        <label className='myinfo-pickphoto' htmlFor='myprofile'>사진 선택하기</label>
                     </div>
                     <input type='file' id='myprofile' name='myprofile' accept='image/*' 
                                  onChange = {handleImageChange} style={{display:'none'}} />       
