0a443ab0d2f2dea98685891f726fbf0efeaff446
조민성 <fjkg33@gmail.com>
2024-07-17 17:15:10 +0900

    footer 간격 조절 및 sns 간격조절

--- src/PetProductsPage/PetItemsPage/ItemsPage.js
+++ src/PetProductsPage/PetItemsPage/ItemsPage.js
@@ -3,34 +3,8 @@ import PetHeader from "../Header/PetHeader";
 import "./PetItemspage.css";
 import PetItems from "../PetItems/Items";
 import Footer from '../Footer/Footer';
+import  mockup from '../mockup'
 
-const mockup = [
-  {
-    id: 1,
-    title: "펀딩중",
-    imgURL: "https://cdn3.wadiz.kr/studio/images/2024/06/13/92daa32e-e953-4f56-ab36-4ce4191d1b5e.jpeg/wadiz/resize/800/format/jpg/quality/85/",
-    name: "[반려견 눈물자국 개선 프로젝트] 저알러지 사료+덴탈껌+트릿 세트!",
-    funding: "20/30 명",
-    FundingPeriod: "7월2일~7월13일",
-  },
-  {
-    id: 2,
-    title: "펀딩중",
-    imgURL: "https://cdn3.wadiz.kr/studio/images/2024/06/04/16f78a9e-930b-4bab-b4fa-1ea6466358b1.jpeg/wadiz/resize/800/format/jpg/quality/85/",
-    name: "딸깍. 켜놓고 출근 하면 끝 | 이제 고양이들의 사냥파티 시작",
-    funding: "20/30 명",
-    FundingPeriod: "7월10일~7월20일",
-  },
-  {
-    id: 3,
-    title: "펀딩중",
-    imgURL:
-      "https://cdn3.wadiz.kr/studio/images/2024/06/06/1790acdb-7c53-4133-80f0-84d1fbea6fe7.jpeg/wadiz/resize/800/format/jpg/quality/85/MockupImg/Mlik.jpga",
-    name: "[5차 신규입고 예약할인] 펫츠온 호주 프리미엄 펫밀크 독앤퍼피/캣앤키튼",
-    funding: "10/30 명",
-    FundingPeriod: "7월2일~7월13일",
-  },
-];
 const PetItemspage = () => {
   return (
     <div>
