fd44995f4cc7adb5cbccd6a329989ff74e6e5994
조민성 <fjkg33@gmail.com>
2024-07-17 16:50:48 +0900

    결제방법 컴퍼넌트 라디오버튼 수정

--- src/PetProductsPage/PayMentPage/PaymentMethod/Payment_Method.js
+++ src/PetProductsPage/PayMentPage/PaymentMethod/Payment_Method.js
@@ -1,39 +1,69 @@
-import React from 'react';
-import "../PaymentMethod/Payment_Method.css"
+import React, { useState } from 'react';
+import "../PaymentMethod/Payment_Method.css";
 import PayMockup from '../PayMockup';
 
-export default function Payment_Method(){
-    return(
-        <>
-             <h3 className='method'>결제방법</h3>
-        <div className='paymethod'>
-            <div className='payment'>
-            <div className='card-payment'>
-                <input type='radio' />
-                <span>신용카드</span>
-            </div>
-            <div className='virtual-account'>
-                <input type='radio' />
-                <span>가상계좌</span>
-            </div>
-            </div>
-            <div className='paymethod-bottom'>
-                <div className='Deposit_without_bankbook'>
-                    <input type='radio' />
-                    <span>무통장입금</span>
-                </div>
-            <div className="Kakao_Pay">
-                <input type='radio' />
-                <span>카카오페이</span>
-            </div>
-            </div>
-            <div className='bank'>
-                <select>
-                    <option>NH은행: {PayMockup.order.bank} 예금주명</option>
-                </select>
-                <input type='text' />
-            </div>
+export default function Payment_Method() {
+  const [selectedMethod, setSelectedMethod] = useState(null);
+
+  const handleRadioChange = (method) => {
+    setSelectedMethod(method);
+    // 필요한 추가 로직을 여기에 작성
+    // 예: 선택된 결제 방법에 따라 다른 컴포넌트 렌더링 등
+  };
+
+  return (
+    <>
+      <h3 className='method'>결제방법</h3>
+      <div className='paymethod'>
+        <div className='payment'>
+          <label className='card-payment'>
+            <input 
+              type='radio' 
+              value="card" 
+              checked={selectedMethod === 'card'} 
+              onChange={() => handleRadioChange('card')} 
+            />
+            <span>신용카드</span>
+          </label>
+          <label className='virtual-account'>
+            <input 
+              type='radio' 
+              value="virtual" 
+              checked={selectedMethod === 'virtual'} 
+              onChange={() => handleRadioChange('virtual')} 
+            />
+            <span>가상계좌</span>
+          </label>
         </div>
-        </>
-    )
+        <div className='paymethod-bottom'>
+          <label className='Deposit_without_bankbook'>
+            <input 
+              type='radio' 
+              value="deposit" 
+              checked={selectedMethod === 'deposit'} 
+              onChange={() => handleRadioChange('deposit')} 
+            />
+            <span>무통장입금</span>
+          </label>
+          <label className="Kakao_Pay">
+            <input 
+              type='radio' 
+              value="kakao" 
+              checked={selectedMethod === 'kakao'} 
+              onChange={() => handleRadioChange('kakao')} 
+            />
+            <span>카카오페이</span>
+          </label>
+        </div>
+        <div className='bank'>
+          <select>
+            <option>NH은행: {PayMockup.order.bank} 예금주명</option>
+          </select>
+          {selectedMethod === 'deposit' && ( 
+            <input type='text' placeholder='예금주명 입력' />
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
