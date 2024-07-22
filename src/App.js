ea05a708450e7b35d9c684c80163efde289c1ee4
xxseoxx <sjm4482@gmail.com>
2024-07-22 10:35:26 +0900

    axios 설치 및 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
+import axios from 'axios';
 import ItemsPage from "./PetProductsPage/PetItemsPage/ItemsPage";
 import ItemDetail from "./PetProductsPage/ItemDetailPage/ItemDetail";
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Main from './MainPage/Main';
-import Login_Page from './login/Login_Page';
-import Join_Page from './join_members/Join_Page';
+import Login from './Login/Login.js';
 import SchoolMain from './School/SchoolMain.js';
 import SchoolPost from './School/SchoolPost.js';
 import EditInfo from './Mypage/EditInfo.js';
@@ -23,8 +23,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login_Page />} />
-          <Route path="/join_members" element={<Join_Page />} />
+          <Route path="/login" element={<Login />} />
            <Route path="/ItemsPage" element={<ItemsPage />} />
           <Route path="/ItemsPage/:imageId" element={<ItemDetail />} />
           <Route path='/school' element={<SchoolMain/>}></Route>
