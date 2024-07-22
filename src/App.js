ec78a5f3b65550884555faef99a5d7887cc54932
xxseoxx <sjm4482@gmail.com>
2024-07-22 10:37:24 +0900

    로그인 토큰

--- src/App.js
+++ src/App.js
@@ -17,6 +17,33 @@ import OrderItem from './PetProductsPage/PayMentPage/OrderItem/OrderItem.js';
 import Withdraw from './Withdraw/Withdraw.js';
 
 function App() {
+
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    if (token) {
+      setAuthToken(token);
+      window.history.replaceState({}, document.title, window.location.pathname); // remove token from URL
+    }
+  }, []);
+
+  const setAuthToken = (token) => {
+    if (token) {
+      localStorage.setItem('token', token);
+      axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+      localStorage.removeItem('token');
+      delete axios.defaults.headers.common['Authorization'];
+    }
+    setToken(token);
+  };
+
+  const handleLogout = () => {
+    setAuthToken('');
+  };
+  
   return (
     <div className="App">
 
@@ -32,9 +59,9 @@ function App() {
           <Route path='/Mypage' element={<Mypage />} />
           <Route path='/Mypage/Header' element={<MypageHeader />} />
           <Route  path="/Funeral" element={<FuneralContainer/>}></Route>
-      <Route  path="/FReservation" element={<ReservationContainer/>}></Route>
-      <Route path='/PayMentPage/:userid'  element={<OrderItem/>} />
-      <Route path='/Witdraw'  element={<Withdraw/>} />
+          <Route  path="/FReservation" element={<ReservationContainer/>}></Route>
+          <Route path='/PayMentPage/:userid'  element={<OrderItem/>} />
+          <Route path='/Witdraw'  element={<Withdraw/>} />
         </Routes>
       </BrowserRouter>
     </div>
