c4b4353af5eb75ed0c62bcc635499067ab8f3bcd
xxseoxx <sjm4482@gmail.com>
2024-07-22 10:57:12 +0900

    Revert "로그인 관련 mypage 수정"
    This reverts commit 9257c4d710565eee3f06ca1ed4120b820c9b5408.

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -7,7 +7,7 @@ export default function Main() {
             <nav className="navFirst">
                 <ul>
                     <li><a href="/mypage">마이페이지</a></li>
-                    <li><Link to="/login">로그인</Link></li>
+                    <li><Link to="/login">로그인&회원가입</Link></li>
                 </ul>
             </nav>
             <h3>Have a puppy day !</h3>
