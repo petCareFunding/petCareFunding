2b65b89ebb3dc890078c98f414f368af1da69ce8
조민성 <fjkg33@gmail.com>
2024-07-23 20:33:30 +0900

    ItemsPage axios 기능 추가 및 변경
    axios 추가로 ItemsPage 코드 변경

--- src/PetProductsPage/PetItemsPage/ItemsPage.js
+++ src/PetProductsPage/PetItemsPage/ItemsPage.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PetHeader from "../Header/PetHeader";
 import "./PetItemspage.css";
-import PetItems from "../PetItems/Items";
-import Footer from '../Footer/Footer';
-import  mockup from '../mockup'
+import PetItem from "../PetItems/Items";
+import Footer from "../Footer/Footer";
+// import mockup from "../mockup";
+import axios from "axios";
 
 const PetItemspage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div>
-      <PetHeader/>
-      <PetItems images={mockup} />
-      <Footer/>
+      <PetHeader />
+        {products.map((product) => (
+          <PetItem key={product.prodid} product={product} />
+        ))}
+      <Footer />
     </div>
   );
 };
