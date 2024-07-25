5992ad5afba214f312852417be3d4b52c2c6489d
Daeuni <ekdmsjj@gmail.com>
2024-07-25 22:06:32 +0900

    메인페이지 install vite, react-spring

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -1,24 +1,19 @@
 import "./Main.css"
 import {Link} from "react-router-dom";
-export default function Main() {
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
+function Main() {
     return(
-        <div className="mainContainer">
-            <h1>MEONG-GORITHM</h1>
-            <nav className="navFirst">
-                <ul>
-                    <li><a href="/mypage">마이페이지</a></li>
-                    <li><Link to='/login'>로그인</Link></li>
-                </ul>
-            </nav>
-            <h3>Have a puppy day !</h3>
-            <nav className="navSecond">
-                <ul>
-                    <li><a href="/school">유치원 펀딩</a></li>
-                    <li><a href="/ItemsPage">반려동물 용품 펀딩</a></li>
-                    <li><a href="/funeral">장묘 예약하기</a></li>
-                    <li><a href="/mypage">내 결제내역 & 찜</a></li>
-                </ul>
-            </nav>
+        <div>
+            <Parallax>
+                <ParallaxLayer>
+                    
+                </ParallaxLayer>
+            </Parallax>
+
         </div>
+        
     )
 };
+
+export default App;
