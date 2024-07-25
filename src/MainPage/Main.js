ef8c7281270f6d7adb11bb70650413fdade81a06
Daeuni <ekdmsjj@gmail.com>
2024-07-25 23:19:55 +0900

    메인 배경 (푸터 x)

--- src/MainPage/Main.js
+++ src/MainPage/Main.js
@@ -4,16 +4,48 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function Main() {
     return(
-        <div>
-            <Parallax>
-                <ParallaxLayer>
-                    
+        <div className="MainPage">
+            <header>
+                
+            </header>
+            <Parallax pages={3}>
+                <ParallaxLayer id="field3" 
+                    style={{backgroundImage:`url(${process.env.PUBLIC_URL}/Main_img/field3.jpg)`}}
+                    offset ={0}
+                    speed={0.3}
+                    factor={4} 
+                >
+                </ParallaxLayer>
+                <ParallaxLayer 
+                    speed={0.3}
+                    sticky={{ start: 0.1, end: 1.2 }}
+                    style={{ textAlign: 'center' }}>
+                    <img src={process.env.PUBLIC_URL + '/Main_img/cat.gif'} />
+                </ParallaxLayer> 
+                <ParallaxLayer  speed={1} className="Main-pupday">
+                    <p id="have">Have a </p>
+                    <p id="puppy">puppy</p> <span id="day">day!</span>
                 </ParallaxLayer>
             </Parallax>
 
+            <footer className="Main-footer">
+                <MainFooter/>
+            </footer>
         </div>
         
     )
 };
 
-export default App;
+
+
+function MainFooter(){
+    return(
+        <div className="footer-meong">
+            <p>MEONG-GORITHM</p>
+        </div>
+    )
+};
+
+
+
+export default Main;
