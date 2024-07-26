07a7c936ff139579c2a6700e3a15c8673948a796
조민성 <fjkg33@gmail.com>
2024-07-26 11:34:16 +0900

    ItemsPage axios 추가 수정

--- src/PetProductsPage/PetItems/Items.js
+++ src/PetProductsPage/PetItems/Items.js
@@ -3,28 +3,34 @@ import "../PetItems/Items.css";
 import { Link } from "react-router-dom";
 
 export default function PetItem({ product }) {
-  const { imgURL, name, funding, fundingperiod, id } = product;
-  //console.log("product", product);
-  console.log("imgURL, name, funding, id ", imgURL, name, funding, id);
+  const { prodImageURL, prodname, funding, fundingperiod, prodid } = product;
+  console.log("product:", product);
+  console.log(
+    "prodImageURL, prodname, funding, prodid: ",
+    prodImageURL,
+    prodname,
+    funding,
+    prodid
+  );
   // ./public/img/dogFood.jpg
-  const imgURL_full = `http://localhost:5000/${imgURL}`;  // http://localhost:5000/dogFood.jpg
-
+  const imgURL_full = `http://localhost:5001/${prodImageURL}`; // http://localhost:5000/dogFood.jpg
 
   return (
     <div className="image-zigzag">
-      <Link to={`/ItemsPage/${id}`}>
-        <div className='pet-img'>
-        <img src={imgURL_full} alt={name} />
-        <div className="item-detail first second">
-          <li className="funding-title">펀딩중</li>
-          <li className="item-name">{name}</li>
-          <li className="item-funding">
-            <span className="funding">펀딩:</span> {funding}명
-          </li>
-          <p className="FundingPeriod">
-            <span className="period">펀딩기간:</span>{fundingperiod}
-          </p>
-        </div>
+      <Link to={`/products/${prodid}`}>
+        <div className="pet-img">
+          <img src={imgURL_full} alt={prodname} />
+          <div className="item-detail first second">
+            <li className="funding-title">펀딩중</li>
+            <li className="item-name">{prodname}</li>
+            <li className="item-funding">
+              <span className="funding">펀딩:</span> {funding}명
+            </li>
+            <p className="FundingPeriod">
+              <span className="period">펀딩기간:</span>
+              {fundingperiod}
+            </p>
+          </div>
         </div>
       </Link>
     </div>
